06812bbad56cd6623590f08bdf09cc443614bc6c
Eir03 <71316160+Eir03@users.noreply.github.com>
2020-12-15 11:24:41 +0300

    Add files via upload

---
+++ Racerrr/app/src/main/java/com/example/racerrr/Data.java
@@ -0,0 +1,6 @@
+package com.example.racerrr;
+
+public class Data {
+    public static String playerName = "Player";
+}
+
---
+++ Racerrr/app/src/main/java/com/example/racerrr/activity_launch_screen.java
@@ -0,0 +1,14 @@
+package com.example.racerrr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_launch_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch_screen);
+    }
+}
